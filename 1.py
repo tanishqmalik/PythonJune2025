7783610552bb1c20fbfe2ef156655ecf51d600bf
tanishqmalik <jobfortanishq@icloud.com>
2025-06-07 03:51:44 +0530

    fc

---
+++ 1.py
@@ -0,0 +1,32 @@
+print("hello world");
+
+x = 5;
+y = 2;
+
+if x > y:
+    print("YES");
+
+# x = str(x);
+
+# print(type(x));
+X = 67.00
+
+x = "John"
+
+print(type(x));
+
+print(x , X)
+
+
+fruits = ["apple", "banana" , "cherry"]
+
+x,y,z = fruits
+
+print(x);
+print(y);
+print(z);
+
+a = b = c = 'hello world'
+
+print(a," ",b,c)
+
---
+++ 2.py
@@ -0,0 +1,49 @@
+# x = " aewsome"
+
+# def myfunc():
+#     print("Python is " + x)
+
+# myfunc();
+
+# x = "awesome"
+
+# def myfunc():
+#     x = "fantastic"
+#     print("Python is " + x)
+
+# myfunc()
+
+# print("python is" + x)
+
+# def myfunc1():
+#     global x
+#     x = "fantastic"
+
+# myfunc1()
+
+# print("Python is " + x)
+
+x = "awesome"
+
+def myfunc1():
+    global x
+    x = "fantastic"
+    print(x)
+myfunc1()
+
+print("Python is" + x);
+
+
+print("""hello jedkda,
+  njdckj Ks
+lkcf d lfczdls""")
+
+a = "Hello, World"
+print(a[1]);
+
+for x in "banana":
+    print(x)
+
+a = " hellojiii"
+
+print(a)
